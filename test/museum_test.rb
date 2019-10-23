560041fd6846c0cf704db79907cf8445431acaec
Dione <dionew1@gmail.com>
2019-10-23 10:29:15 -0600

    Remove skip from tests 1 & 2

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,13 +5,11 @@ require './lib/museum'
 class MuseumTest <Minitest::Test
 
   def test_it_exists
-    skip
     dmns = Museum.new("Denver Museum of Nature and Science")
     assert_instance_of Museum, dmns
   end
 
   def test_it_has_attributes
-    skip
     dmns = Museum.new("Denver Museum of Nature and Science")
 
     assert_equal "Denver Museum of Nature and Science", dmns.name
